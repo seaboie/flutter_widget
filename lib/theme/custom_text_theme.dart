e73d969b2221c44a807d07db0d5702971545495d
kritbovorn <kritbovorn@gmail.com>
2023-07-11 22:42:15 +0700

    add: Theme()

---
+++ lib/theme/custom_text_theme.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var customTextTheme = TextTheme(
+  titleSmall: GoogleFonts.notoSansThai(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.2,
+  ),
+  titleMedium: GoogleFonts.notoSansThai(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.2,
+  ),
+  titleLarge: GoogleFonts.notoSansThai(
+    fontSize: 12,
+    fontWeight: FontWeight.w800,
+    letterSpacing: 0.2,
+  ),
+  labelSmall: GoogleFonts.notoSansThai(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.2,
+  ),
+  labelMedium: GoogleFonts.notoSansThai(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.2,
+  ),
+  labelLarge: GoogleFonts.notoSansThai(
+    fontSize: 14,
+    fontWeight: FontWeight.w800,
+    letterSpacing: 0.2,
+  ),
+  bodySmall: GoogleFonts.notoSansThai(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.2,
+  ),
+  bodyMedium: GoogleFonts.notoSansThai(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.2,
+  ),
+  bodyLarge: GoogleFonts.notoSansThai(
+    fontSize: 16,
+    fontWeight: FontWeight.w800,
+    letterSpacing: 0.2,
+  ),
+  displaySmall: GoogleFonts.notoSansThai(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.2,
+  ),
+  displayMedium: GoogleFonts.notoSansThai(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.2,
+  ),
+  displayLarge: GoogleFonts.notoSansThai(
+    fontSize: 20,
+    fontWeight: FontWeight.w800,
+    letterSpacing: 0.2,
+  ),
+  headlineSmall: GoogleFonts.notoSansThai(
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.2,
+  ),
+  headlineMedium: GoogleFonts.notoSansThai(
+    fontSize: 22,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.2,
+  ),
+  headlineLarge: GoogleFonts.notoSansThai(
+    fontSize: 22,
+    fontWeight: FontWeight.w800,
+    letterSpacing: 0.2,
+  ),
+);
